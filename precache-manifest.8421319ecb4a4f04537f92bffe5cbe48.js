7c473beada0646c9af93994e572a3f8109a098dc
Jairo Perdomo <jaanpeto_wx@hotmail.com>
2020-08-17 12:08:03 -0500

    Updates

---
+++ precache-manifest.8421319ecb4a4f04537f92bffe5cbe48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc1951c2b00df9b81a8a19eb4b20411",
+    "url": "/monsters.rolodex/index.html"
+  },
+  {
+    "revision": "992e51f413798edcf48b",
+    "url": "/monsters.rolodex/static/css/main.8f6151ba.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters.rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters.rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992e51f413798edcf48b",
+    "url": "/monsters.rolodex/static/js/main.2e01c65e.chunk.js"
+  },
+  {
+    "revision": "4b1b92399468a73c4316",
+    "url": "/monsters.rolodex/static/js/runtime-main.ac5aa774.js"
+  }
+]);
